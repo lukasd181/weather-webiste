9d7cbad3404d94e99da923f39be7fa1088cd3fdb
Lukas <hungduonggia181@gmail.com>
2020-09-16 13:39:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null
+    }
+  }
+  getWeather = async() => {
+    let apikey = process.env.REACT_APP_APIKEY;
+    let url =`https://api.openweathermap.org/data/2.5/weather?q=schenectady&appid=${apikey}&units=metric`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+    this.setState({weather:data});
+
+
+
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+  render() {
+    return (
+      <div>
+        {this.state.weather && this.state.weather.main.temp}
+      </div>
+    )
+  }
+}
+
